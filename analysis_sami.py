f417dee1eae5d9d9d806fd2faecf4f53c6360fd2
Claudia Lagos <claudia.lagos@icrar.org>
2020-08-06 17:16:11 +0800

    Adding required data to run scripts

--- analysis_sami.py
+++ analysis_sami.py
@@ -10,14 +10,15 @@ xmf = mbins + dm/2.0
 catID, lambdaR_SAMI_nocorr = np.loadtxt('jvds_stelkin_cat_v012_mge_private.dat', unpack=True, usecols=[0, 41])
 ids_table = np.arange(0,len(lambdaR_SAMI_nocorr))
 mstar_SAMI, lambdaR_SAMI, ellip_SAMI, sigma_SAMI = np.loadtxt('jvds_stelkin_cat_v012_mge_seecorr_kh20_v190620_private.dat', unpack=True, usecols=[6, 41, 15, 35])
-gn, sgn, mstar_eag, lambdaR_eag, ellip_eag, sigma_eag = np.loadtxt('LambdaGalaxiesz0_RefL100N1504.dat', unpack=True, usecols=[0,1,2,5,12,14])
+gn, sgn, mstar_eag, lambdaR_eag_eo, lambdaR_eag, ellip_eag_eo, ellip_eag, sigma_eag = np.loadtxt('LambdaGalaxiesz0_RefL100N1504.dat', unpack=True, usecols=[0,1,2,4,5,11,12,14])
+galidl = np.loadtxt('GalaxyIDsLambdaGalaxiesz0_RefL100N1504.dat', unpack=True, usecols=[0])
 
 
 massbins = [10.0,10.1,10.2,10.3,10.4,10.5,10.6,10.7,10.8,10.9]
 ind = np.where((mstar_SAMI >= 10) & (mstar_SAMI <=11))
 n_gals = len(mstar_SAMI[ind])
 
-props_selected_eagle = np.zeros(shape = (4,n_gals))
+props_selected_eagle = np.zeros(shape = (5,n_gals))
 
 g = 0
 for i in range(0,len(massbins)-1):
@@ -29,6 +30,7 @@ for i in range(0,len(massbins)-1):
     lambdaR_eagin = lambdaR_eag[ind]
     ellip_eagin = ellip_eag[ind]
     sigma_eagin = sigma_eag[ind]
+    galid_in = galidl[ind]
     ids = np.arange(len(mstarin))
     selected = np.random.choice(ids, size=len_sami)
     for j in range(0,len(selected)):
@@ -36,6 +38,7 @@ for i in range(0,len(massbins)-1):
         props_selected_eagle[1,g+j] = lambdaR_eagin[selected[j]]
         props_selected_eagle[2,g+j] = ellip_eagin[selected[j]]
         props_selected_eagle[3,g+j] = sigma_eagin[selected[j]]
+        props_selected_eagle[4,g+j] = galid_in[selected[j]]
 
     g = g + len(selected)
 
@@ -149,9 +152,9 @@ ind = np.where((mstar_SAMI >= 10) & (mstar_SAMI <= 10.9) & (lambdaR_SAMI >= 0) &
 ax.hist(sigma_SAMI[ind], bins=20, range=(50,240), density=True, facecolor='blue', histtype='step', linewidth=2, alpha=0.5, fill=True, edgecolor='k',label='SAMI')
 ind = np.where((mstar_SAMI >= 10) & (mstar_SAMI <= 10.9) & (lambdaR_SAMI >= 0) & (lambdaR_SAMI_nocorr <= 0.08 + ellip_SAMI/4.0) & (ellip_SAMI <=0.4) & (sigma_SAMI > 0))
 ax.hist(sigma_SAMI[ind], bins=20, range=(50,240), density=True, edgecolor='blue', histtype='step', linewidth=2, alpha=0.85, label='SAMI no corr')
-print(max(sigma_SAMI[ind]))
 ind = np.where((props_selected_eagle[0,:] >= 10) & (props_selected_eagle[0,:] <= 10.9) & (props_selected_eagle[1,:] <= 0.08 + props_selected_eagle[2,:]/4.0) & (props_selected_eagle[2,:] <= 0.4))
 ein = props_selected_eagle[3,ind]
+galidsmatched = props_selected_eagle[4,ind]
 ax.hist(ein[0], bins=20, range=(60,180), density=True, facecolor='red', histtype='step', linewidth=2, alpha=0.25, fill=True, edgecolor='k', label='EAGLE')
 
 common.savefig(obsdir, fig, "lambdaR_Eps_plane.pdf")
@@ -160,13 +163,45 @@ common.savefig(obsdir, fig, "lambdaR_Eps_plane.pdf")
 
 #################################################### analysis of classification ###########################################
 
+#match GalaxyId with Group and SubGroupNumber
+def match_galid(gnl, sgnl):
+    galid, gn, sgn = np.loadtxt('/opt/claudia/EAGLE/Selection_RefL100N1504_z0.dat', unpack=True, usecols=[0,1,2])
+    galid_to_write = np.zeros(shape = len(gnl))
+    g=0
+    for a,b in zip(gnl, sgnl):
+        match = np.where((gn == a) & (sgn == b))
+        if(len(gn[match]) > 0):
+           galid_to_write[g] = galid[match]
+           g = g + 1
+    return galid_to_write
+
+def obtain_ellipticity_and_lambda(galid):
+    props = np.zeros(shape = (2,len(galid)))
+    for i in range(0,len(galid)):
+        match = np.where(galidl == galid[i])
+        if(len(galidl[match]) > 0):
+           props[0,i] = lambdaR_eag_eo[match]
+           props[1,i] = ellip_eag_eo[match]
+    return props
+
 #read table with classification
 galid, class1, class2, class3 = np.loadtxt('Classification/Classification.txt', unpack=True, usecols=[0,1,3,5])
+commonids = np.in1d(galid, galidsmatched)
+matched = np.where(commonids == True)
+#print(galid[matched])
+
+all_classified_srs = np.in1d(galidl, galid)
+#print(all_classified_srs)
+#for a,b,c in zip(galid, ellip_eag_eo[all_classified_srs], ellip_eag[all_classified_srs]):
+#    print(a,b,c)
+
+
 classification = np.zeros(shape = (len(galid), 3))
 classification[:,0] = class1
 classification[:,1] = class2
 classification[:,2] = class3
 
+props = obtain_ellipticity_and_lambda(galid)
 #determine the number of galaxies that have three matches, two matches, or all different answers, use flags=2, 1, 0 to determine this
 successmatching = np.zeros(shape = (len(galid)))
 successmatching[:] = -1
@@ -178,6 +213,9 @@ for i in range(0,len(successmatching)):
         successmatching[i] = 1
     elif((class1[i] != class2[i]) & (class2[i] != class3[i]) & (class1[i] != class3[i])):
         successmatching[i] = 0
+
+###############################################################################3
+#plot success rate of matching procedure
 
 xtit="Matching success"
 ytit="N"
@@ -192,6 +230,10 @@ ax.hist(successmatching, bins=3, range=(-0.5,2.5), facecolor='blue', histtype='s
 plt.xticks([0,1,2], x_values)
 obsdir = '/opt/claudia/ObsData/SAMI/Classification/Plots/'
 common.savefig(obsdir, fig, "MatchesInClass.pdf")
+
+
+#################################################################
+#plot distribution of kinematic classes for the three match success %
 
 xtit="Kinematic class"
 ytit="PDF"
@@ -224,10 +266,122 @@ for i in range(0,len(matches)):
 obsdir = '/opt/claudia/ObsData/SAMI/Classification/Plots/'
 common.savefig(obsdir, fig, "KinClassVsMatchingSuccess.pdf")
 
+###################################################################
+#plot ellipticity distribution for slow rotators in classes 0 and 1
+xtit="$\\epsilon_{\\rm r_{50},edge-on}$"
+ytit="N"
+fig = plt.figure(figsize=(3,5))
+plt.subplots_adjust(left=0.25, bottom=0.17)
+xmin, xmax, ymin, ymax = 0, 0.6, 0, 30
+subplots = (211, 212)
+matches = (0, 1)
+labels = ('FSR', 'RSR')
+xpos, ypos = xmin + 0.1 * (xmax - xmin)/2.0, ymax - 0.15 * (ymax - ymin)
+def plot_elip_class(ax, classification, match=0, color='red'):
+    ind = np.where(classification[:] == match)
+    ein = props[1,ind]
+    ax.hist(ein[0], bins=10, range=(0,0.6), facecolor=color, histtype='step', linewidth=2, alpha=0.5, fill=True, edgecolor='k')
 
-print(successmatching)
+for i in range(0,len(matches)):
+    ax = fig.add_subplot(subplots[i])
+    common.prepare_ax(ax, xmin, xmax, ymin, ymax, xtit, ytit, locators=(0.1, 0.1, 5, 5))
+    ax.text(xpos, ypos, 'Kin class = %s' % labels[i])
+    plot_elip_class(ax, class1, match=matches[i], color='orange')
+    plot_elip_class(ax, class2, match=matches[i], color='red')
+    plot_elip_class(ax, class3, match=matches[i], color='salmon')
+
+obsdir = '/opt/claudia/ObsData/SAMI/Classification/Plots/'
+common.savefig(obsdir, fig, "EllipticityInClass.pdf")
 
 
+###################################################################################
+###################################################################################
+###################################################################################
+# Analysis of merger history of different kinematic classes
+
+mergerdata = np.loadtxt('MergerHistoryCumulative.dat')
+print mergerdata.shape
+def find_classification(selective = True):
+    if(selective):
+       matchedobjs = np.where(successmatching >= 0)
+       classificationin = classification[matchedobjs,:]
+       classificationin = classificationin[0]
+       finalclass = np.zeros(shape = len(galid[matchedobjs]))
+       finalgalid = galid[matchedobjs]
+       for i in range(0, len(galid[matchedobjs])):
+           finalclass[i] = np.median(classificationin[i,:])
+       print(finalclass)
+       matched = np.in1d(galidl, finalgalid)
+       match = np.where(matched == True)
+       finalgn = gn[match]
+       finalsgn = sgn[match]
+       return(finalclass, finalgn, finalsgn, finalgalid)
+    else:
+       matched = np.in1d(galidl, galid)
+       match = np.where(matched == True)
+       finalgn = gn[match]
+       finalsgn = sgn[match]
+       return(class3, finalgn, finalsgn, galid)
+
+
+finalclass, finalgn, finalsgn, finalgalid = find_classification(selective = True)
+
+
+mergerhist_srs = np.zeros(shape = (12,len(finalgn)))
+for i in range(0,len(finalgn)):
+    match = np.where((mergerdata[:,0] == finalgn[i]) & (mergerdata[:,1] == finalsgn[i]))
+    mergerhist_srs[0:8,i] = mergerdata[match,2:10]
+    mergerhist_srs[8,i] = np.sum(mergerdata[match,3:6]) #all minor mergers
+    mergerhist_srs[9,i] = np.sum(mergerdata[match,6:9]) #all major mergers
+    mergerhist_srs[10,i] = mergerdata[match,3] + mergerdata[match,6] #dry mergers
+    mergerhist_srs[11,i] = mergerdata[match,4] + mergerdata[match,5] + mergerdata[match,7] + mergerdata[match,8] #wet mergers
+
+print(mergerhist_srs[:,0])
+############################
+# plot distribution of kinematic classes depending on merger history
+subplots = (611, 612, 613, 614, 615, 616)
+mergers_explore = (9, 8, 0, 7, 10, 11)
+labels = ('Major mergers', 'Minor mergers', 'Very minor mergers', 'No mergers', 'Dry mergers', 'Wet mergers')
+xtit="Kinematic class"
+ytit="N"
+fig = plt.figure(figsize=(3.5,8))
+plt.subplots_adjust(left=0.25, bottom=0.17)
+x_values = ['FSR', 'RSR', '2$\sigma$', 'Prol','Uncl','R']
+colors = ['k', 'darkgreen', 'blue', 'red', 'Navy', 'Yellow']
+xmin, xmax, ymin, ymax = -0.5, 5.5, 0, 60
+
+xpos, ypos = xmax - 0.63 * (xmax - xmin), ymax - 0.15 * (ymax - ymin)
+
+def plot_kin_class_mergers(ax, classi, color='red'):
+    ax.hist(classi, bins=6, range=(-0.5,5.5), facecolor=color, histtype='step', linewidth=2, alpha=0.5, fill=True, edgecolor='k')
+
+for  i in range(0,len(mergers_explore)):
+    ax = fig.add_subplot(subplots[i])
+    common.prepare_ax(ax, xmin, xmax, ymin, ymax, xtit, ytit, locators=(1, 1, 15, 15))
+    ax.text(xpos, ypos, labels[i])
+    if i == 0:
+       ind = np.where(mergerhist_srs[mergers_explore[i],:] > 0)
+    elif i == 1:
+       ind = np.where((mergerhist_srs[mergers_explore[i-1],:]== 0) & (mergerhist_srs[mergers_explore[i],:] > 0))
+    elif i == 2:
+       ind = np.where((mergerhist_srs[mergers_explore[i-2],:]== 0) & (mergerhist_srs[mergers_explore[i-1],:]== 0) & (mergerhist_srs[mergers_explore[i],:] > 0))
+    elif i == 3:
+       ind = np.where(mergerhist_srs[mergers_explore[i],:] == 0)
+    elif i == 4:
+       ind = np.where(mergerhist_srs[mergers_explore[i],:] > 0)
+    elif i == 5:
+       ind = np.where((mergerhist_srs[mergers_explore[i-1],:] == 0) & (mergerhist_srs[mergers_explore[i],:] > 0))
+
+    plot_kin_class_mergers(ax, finalclass[ind], color=colors[i])
+    if(i <= 4):
+       plt.xticks([0,1,2,3,4,5]," ")
+    else:
+       plt.xticks([0,1,2,3,4,5], x_values)
+
+common.savefig(obsdir, fig, "MergerHistoryDistributionKinClasses.pdf")
+
+
+   
 
 
 
